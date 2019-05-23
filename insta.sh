76b3c4eff6dbeddcb8404c8e34249199ecca5241
IyXerXez7 <50941056+IyXerXez7@users.noreply.github.com>
2019-05-23 14:32:25 +0700

    Update insta.sh

--- insta.sh
+++ insta.sh
@@ -1,4 +1,4 @@
-##
+##gilang no recode asu
 bi='\033[34;1m' #biru
 i='\033[32;1m' #ijo
 pur='\033[35;1m' #purple
@@ -9,8 +9,8 @@ ku='\033[33;1m' #kuning
 echo ""
 clear
 figlet "XerXez7 Gans" | lolcat
-echo -e "╭─NAMA KAMU SIAPA"|lolcat
-read -p '╰─$ ' nam;
+echo -e "â•­â”€NAMA KAMU SIAPA"|lolcat
+read -p 'â•°â”€$ ' nam;
 echo $red"          Tool akan di install dalam waktu 3 detik"
 echo $red"                         SABAR GAYN"
 sleep 1
@@ -62,26 +62,26 @@ pkg update && pkg upgrade -y
 sleep 1
 echo $me ""
 date | lolcat | screenfetch -A Pardus
-echo "(×)-------------------------(×)"|lolcat
-echo "(×)~°Install MINI SHELL     (×)"|lolcat
-echo "(×)-!AUTHOR = XERXEZ7       (×)"|lolcat
-echo "(×)-!TEAM =2E4H-LIT-DFA-GBLK(×)" |lolcat
-echo "(×)-!SINCE =2K19-2K20       (x)"|lolcat
-echo "(×)-!Wa = 18+ 8588××××741   (×)"|lolcat
-echo "(×)-!InYourXerXez7          (x)"|lolcat
-echo "(×)-------------------------(×)"|lolcat
+echo "(Ã—)-------------------------(Ã—)"|lolcat
+echo "(Ã—)~Â°Install MINI SHELL     (Ã—)"|lolcat
+echo "(Ã—)-!AUTHOR = XERXEZ7       (Ã—)"|lolcat
+echo "(Ã—)-!TEAM =2E4H-LIT-DFA-GBLK(Ã—)" |lolcat
+echo "(Ã—)-!SINCE =2K19-2K20       (x)"|lolcat
+echo "(Ã—)-!Wa = 18+ 8588Ã—Ã—Ã—Ã—741   (Ã—)"|lolcat
+echo "(Ã—)-!InYourXerXez7          (x)"|lolcat
+echo "(Ã—)-------------------------(Ã—)"|lolcat
 echo ""
-echo -e "╭─Root@XerXez7"
-read -p "╰─Y/N $ " cie
+echo -e "â•­â”€Root@XerXez7"
+read -p "â•°â”€Y/N $ " cie
 
 if [ $cie = Y ] || [ $cie = y ]
 then
 clear
 figlet "XerXez7 Gans" | lolcat
-echo -e "╭─NAMA KAMU SIAPA"|lolcat
-read -p '╰─$ ' nam;
-echo -e "╭─MASUKAN AKSES DIRECKTORI PENYIMPANAN"|lolcat
-read -p '╰─$ ' eww;
+echo -e "â•­â”€NAMA KAMU SIAPA"|lolcat
+read -p 'â•°â”€$ ' nam;
+echo -e "â•­â”€MASUKAN AKSES DIRECKTORI PENYIMPANAN"|lolcat
+read -p 'â•°â”€$ ' eww;
 cp -f sending.php $eww
 echo " $nam cek di $eww/sending.php"|lolcat
 sleep 10
