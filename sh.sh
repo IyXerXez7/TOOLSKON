1740843afe2e20ad1362a9d143d52badadc56f5b
IyXerXez7 <50941056+IyXerXez7@users.noreply.github.com>
2019-05-23 13:01:18 +0700

    Add files via upload

---
+++ sh.sh
@@ -0,0 +1,123 @@
+##
+##
+##
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo ""
+clear
+figlet "XerXez7 Gans" | lolcat
+echo "MOHON LAYAR TERMUX DI PERKECIL KARMA MENJAGA KESETABILAN HEHE"|lolcat
+echo -e "╭─NAMA KAMU SIAPA"|lolcat
+read -p '╰─$ ' nam;
+echo $red"          Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v("   
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                            _|_|_ '''
+sleep 1
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+pkg update && pkg upgrade
+pkg install mc
+pkg install screenfetch
+pkg install neofetch
+pkg install ruby
+pkg install toilet
+gem install lolcat
+pkg install figlet
+sleep 1
+sleep 1
+clear
+sleep 1
+clear
+echo ""
+date | lolcat | screenfetch -A Linux
+sleep 10
+echo "HAY $nam MAAF YA TOOLSYA BRANTAKAN HEHE"|lolcat
+echo "HARGAILAH WALAU BRANTAKAN TOOLS INI HASIL" | lolcat
+echo "SAYA SEBDIRI MAAF KALO TOOLS.INI GA BERGUNA"|lolcat
+echo
+echo "$ku |×××××××××××××××××|$i |××××××××××××××××|"
+echo "$me |Webdav= (I/i)|      $pur |♤Shell = (S/s)|"
+echo "$ku |◇Insta= (N/n)|      $pu |Exit = (X/x)  |"
+echo "$me |            DDOS (D/d)                 |"|lolcat
+echo "$ku |×××××××××××××××××|$i |××××××××××××××××|"
+echo -e "╭─Root@XerXez7"
+read -p "╰─$ " asw
+
+
+if [ $asw = I ] || [ $asw = i ]
+then
+figlet IyXerXez7|lolcat
+sh n.sh
+fi
+
+
+if [ $asw = N ] || [ $asw = n ]
+then
+sh i.sh
+fi
+
+
+if [ $asw = S ] || [ $asw = s ]
+then
+sh insta.sh
+fi
+
+if [ $asw = X ] || [ $asw = x ]
+then
+toilet -f big EXIT | lolcat
+exit
+fi
+
+if [ $asw = D ] || [ $asw = d ]
+then
+pkg updat && upgrade
+git clone https://github.com/gillanggans7/DdosXerXez7
+cd DdosXerXez7
+python2 ase.py
+fi
